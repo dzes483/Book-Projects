14608a773d5a6709d91a2a9bd576c5a3887b5157
dzes483 <jban@sdf.org>
2020-04-26 15:59:16 +0300

    PEP8 style fixes

--- Chapter 12/blank_row_inserter.py
+++ Chapter 12/blank_row_inserter.py
@@ -1,14 +1,15 @@
 #! python3
-# blank_row_inserter.py - Takes two integers and a filename string as arguments.
-# Starting at row num (first integer), the program inserts num_rows (second integer)
-# blank rows into the spreadsheet.
+# blank_row_inserter.py - Takes two integers and a filename string as
+# arguments. Starting at row num (first integer), the program inserts num_rows
+# (second integer) blank rows into the spreadsheet.
 
 import openpyxl
 
 # Get the user's input
 while True:
     while True:
-        file_name = input('Enter the filename (the final document will be saved as a copy): ')
+        file_name = input('Enter the filename (the final document will be '
+                          'saved as a copy): ')
         if file_name.endswith('.xlsx'):
             break
         else:
@@ -19,7 +20,8 @@ while True:
             num = int(input('Enter the row number to begin at: '))
             num_rows = int(input('Enter the number of rows to insert: '))
         except ValueError:
-            print("You've entered an invalid character. Please enter a number. ")
+            print("You've entered an invalid character."
+                  " Please enter a number.")
             continue
         else:
             break
@@ -28,7 +30,8 @@ while True:
     try:
         wb_source = openpyxl.load_workbook(file_name)
     except FileNotFoundError:
-        print("We couldn't find that file. Check the spelling and that it exists in the current directory")
+        print("We couldn't find that file. Check the spelling and that it "
+              "exists in the current directory.")
         continue
     else:
         break
@@ -37,5 +40,5 @@ source_sheet = wb_source.active
 
 source_sheet.insert_rows(num, amount=num_rows)
 
-wb_source.save('Copy of '+ file_name)
+wb_source.save('Copy of ' + file_name)
 print('New file saved.')
