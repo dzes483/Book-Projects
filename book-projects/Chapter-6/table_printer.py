ea38c5375764a0cd16edada5addc3fcf8003c99d
Jessica <jban@sdf.org>
2020-05-16 20:53:16 +0300

    Clarify comments, fix shebang line

--- book-projects/Chapter-6/table_printer.py
+++ book-projects/Chapter-6/table_printer.py
@@ -1,4 +1,4 @@
-#! python3
+#!/usr/bin/python3
 # table_printer.py - Takes a list of lists of strings and displays it in a
 # well-organized table with each column right-justified.
 
@@ -8,11 +8,11 @@ table_data = [['apples', 'oranges', 'cherries', 'bananas'],
 
 
 def printTable(data):
-    for j in range(len(data[0])):
-        for i in range(len(data)):
+    for i in range(len(data[0])):
+        for j in range(len(data)):
             # This line finds the longest item for each list and its length
-            x = len(max(data[i], key=len))
-            print(data[i][j].rjust(x), end=' ')
+            x = len(max(data[j], key=len))
+            print(data[j][i].rjust(x), end=' ')
         print()
 
 printTable(table_data)
