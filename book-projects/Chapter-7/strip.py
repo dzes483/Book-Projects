7b5e97ff49b03fe09777144625c9cd8c1c423756
Jessica <jban@sdf.org>
2020-05-19 19:56:35 +0300

    Add line breaks to README.md

--- book-projects/Chapter-7/strip.py
+++ book-projects/Chapter-7/strip.py
@@ -1,17 +1,21 @@
 #! python3
-# strip.py - A function version of the strip() string method.
+# strip.py - A function version of the strip() string method. It can replace
+# the spaces with a character or characters of the user's choice.
+
 import re
 
-strip_left = re.compile(r'^\s*')
-strip_right = re.compile(r'\s*$')
+strip_left = re.compile(r'^\s*')    # all space characters to the left
+strip_right = re.compile(r'\s*$')   # all space characters to the right
 strip = re.compile(r'^\s*|\s*$')
 
 
 def strip_func(str, char=None):
-    if not char:
+    if not char:        # if no character is specified, just strip the string
+        # new_str = re.sub(strip_left, "", str)
+        # new_str = re.sub(strip_right, "", new_str)
         new_str = re.sub(strip, "", str)
         return new_str
-    else:
+    else:               # otherwise, replace the whitespace with the character
         new_str = re.sub(r'^\s*|\s*$', char, str)
         return new_str
 
