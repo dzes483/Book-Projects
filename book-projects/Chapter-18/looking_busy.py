2f138053a8bbdeff490d84af3e14876a99973ad5
Jessica <jban@sdf.org>
2020-05-29 20:59:16 +0300

    Add new file "looking_busy.py"

---
+++ book-projects/Chapter-18/looking_busy.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python3
+# looking_busy.py - Nudges the mouse every ten seconds to prevent the display
+# of an idle status in messaging programs.
+
+import pyautogui
+import time
+import sys
+
+print('Looking busy! Press Ctrl + C at any time to quit.')
+while True:
+    try:
+        time.sleep(10)
+        pyautogui.moveRel(10, 0, duration=0.25)
+        time.sleep(10)
+        pyautogui.moveRel(0, 10, duration=0.25)
+        time.sleep(10)
+        pyautogui.moveRel(-10, 0, duration=0.25)
+        time.sleep(10)
+        pyautogui.moveRel(0, -10, duration=0.25)
+        time.sleep(10)
+    except KeyboardInterrupt:
+        sys.exit()
